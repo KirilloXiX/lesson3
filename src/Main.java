23b5d19f10a689bc54579fa798a6a4c7de1f141d
Kirill <vovk.kiryusha.007@gmail.com>
2023-07-13 12:39:03 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println("****  Name: Kirill  ****");
         System.out.println("****  Skype: kirixoz  ****");
         System.out.println("*********************");
+        System.out.println("*some changes*");
+
 
 
 
