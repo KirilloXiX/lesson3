a3fe80a9420067ad91310f5fd60e9ed0fd302954
Kirill <vovk.kiryusha.007@gmail.com>
2023-07-13 12:32:19 +0300

    add hw 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("*********************");
+        System.out.println("***  business card  ***");
+        System.out.println("****  Name: Kirill  ****");
+        System.out.println("****  Skype: kirixoz  ****");
+        System.out.println("*********************");
+
+
+
+    }
+}
